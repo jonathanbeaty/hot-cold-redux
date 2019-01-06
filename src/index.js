be224e1070f94977dbbb969ed99b5bba249ab4b1
Jonathan Beaty <jonathanbeaty15@gmail.com>
2019-01-06 16:02:30 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
-import './reset.css';
-import './index.css';
+import store from "./store";
 
-import Game from './components/game';
+import "./reset.css";
+import "./index.css";
+
+import Game from "./components/game";
 
 ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
+  <Provider store={store}>
+    <Game />
+  </Provider>,
+  document.getElementById("root")
 );
